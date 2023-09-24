10265328ef6c1b97370325452b4e17103821075b
Akhil_teja <99877317+akhilteja06@users.noreply.github.com>
2023-09-24 10:20:31 +0530

    Add files via upload

---
+++ New folder/terraform/main.tf
@@ -0,0 +1,142 @@
+#EC2
+resource "aws_instance" "wordpress" {
+  ami                         = "ami-0f5ee92e2d63afc18"
+  instance_type               = "t2.micro"
+  key_name                    = "key-pair"
+  subnet_id                   = aws_subnet.public1.id
+  security_groups             = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
+
+}
+
+#rds subnet
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
+}
+#RDS INSTANCE
+resource "aws_db_instance" "rds_instance" {
+  engine                    = "mysql"
+  engine_version            = "5.7"
+  skip_final_snapshot       = true
+  final_snapshot_identifier = "my-final-snapshot"
+  instance_class            = "db.t2.micro"
+  allocated_storage         = 20
+  identifier                = "my-rds-instance"
+  db_name	                  = "wordpress_db"
+  username                  = "akhil"
+  password                  = "akhilteja$$$123"
+  db_subnet_group_name      = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids    = [aws_security_group.rds_security_group.id]
+
+}
+# RDS security group
+resource "aws_security_group" "rds_security_group" {
+  name        = "rds-security-group"
+  description = "Security group for RDS instance"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.100.0.0/16"]
+  }
+
+}
+
+
+
+#internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+
+}
+
+#elastic ip
+resource "aws_eip" "eip" {
+  domain = "vpc"
+}
+
+#nat gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public1.id
+
+}
+
+#public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+}
+
+#private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+}
+
+
+#route table association
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
+}
+
+#security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
